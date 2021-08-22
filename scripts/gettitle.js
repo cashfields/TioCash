bcaacfc9dcb1d84f03751847aa23d672d38b2543
Scott CA$H Fields <cashfieldsutc@gmail.com>
2021-08-22 10:38:11 -0500

    Create gettitle.js

---
+++ scripts/gettitle.js
@@ -0,0 +1,18 @@
+function myFunction() 
+{
+  alert("Here Top");
+var theHead = document.head;
+console.log(theHead.id); // "my-document-head";
+console.log( theHead === document.querySelector("head") ); // true
+//alert(theHead.id);
+//alert("skip")
+var wantedname = (theHead.id);
+alert(wantedname);
+document.getElementById("mywantedname").innerHTML = wantedname;
+
+var x = document.title;
+alert(x);
+document.getElementById("myx").innerHTML = x
+console.log(x); 
+  alert("Here Bot");
+}
